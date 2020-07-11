7c27527f26367698fb568f2bb741fc0221081562
Dan Hromada <hromada.dan@gmail.com>
2020-07-11 14:06:44 +0200

    Add AWS Amplify Core and Auth

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+import { Amplify } from '@aws-amplify/core';
+import { Auth } from '@aws-amplify/auth';
+
 import('./App').then(module => render(module.default));
 
+
+Amplify.configure(require('./aws-exports')); // Cognito, ...
 
 
 function render(App) {
